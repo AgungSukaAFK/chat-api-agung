33c0c3423d712b0b8c69fe59d61c0a41515b31c6
AgungSukaAFK <wismaagung23@gmail.com>
2023-09-18 14:42:21 +0700

    Nambahin cors config

--- src/index.js
+++ src/index.js
@@ -47,6 +47,13 @@ app.use(session({
     }
 }))
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  })
+
 app.listen(4000, () => {
     console.log(`Service listening on PORT 4000 ...`)
 })
